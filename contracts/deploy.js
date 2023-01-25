b34a597567cf791f2cfe109a7e2be6441232fef4
Yorkzhong1 <yorkzhong@gmail.com>
2023-01-25 10:59:54 -0600

    added contract

---
+++ contracts/deploy.js
@@ -0,0 +1,32 @@
+//local: 0xce7f0ebA495983DB9b5983306F0F2CC417b64a6D
+//goerli: Exchange Contract Address: 0x4F737D67210fF3aF1FF32B83Cb91372833427D24
+
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS } = require("../constants");
+
+async function main() {
+  const cryptoDevTokenAddress = CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so exchangeContract here is a factory for instances of our Exchange contract.
+  */
+  const exchangeContract = await ethers.getContractFactory("Exchange");
+
+  // here we deploy the contract
+  const deployedExchangeContract = await exchangeContract.deploy(
+    cryptoDevTokenAddress
+  );
+  await deployedExchangeContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("Exchange Contract Address:", deployedExchangeContract.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
